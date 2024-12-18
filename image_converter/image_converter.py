46ba615ebf99b08e7d6b31e785e046c6a9f4813c
Kevin Hernandez <kevinhernem@gmail.com>
2024-12-18 16:46:16 +0900

    feat: implement parser to use as a batch script on the terminal

--- image_converter/image_converter.py
+++ image_converter/image_converter.py
@@ -17,13 +17,13 @@ def convert_image(input_file, output_file=None, output_format="png"):
         output_image_path = ""
 
         if output_file == None:
-            output_image_path = input_image_path.replace(".HEIC", f".{output_format.lower()}")
+            output_image_path = input_image_path.replace(".HEIC", f".{output_format}")
         else:
-            output_image_path = f"{input_image_path.split('.', 1)[0]}.{output_format}"
-
+            output_image_path = f"{output_file}.{output_format}"
+        
         temp_img.save(output_image_path)
 
-        print(f"{input_file} was converted succesfully into {output_image_path}")
+        print(f"\n{input_file} was converted succesfully into {output_image_path}")
 
     else:
         print("This file format is not supported yet:(")
@@ -32,7 +32,18 @@ def convert_image(input_file, output_file=None, output_format="png"):
 
 
 def main():
-    convert_image("IMG_1815")
+    parser = argparse.ArgumentParser(description="Convert Image Formats")
+    parser.add_argument("input_file", type=str, help="Name of Input Image")
+    parser.add_argument("-o", "--output_file", type=str, help="Name of output image", default=None)
+    parser.add_argument("-f", "--format", type=str, help="Format of Image after Conversion", default="png")
+    args = parser.parse_args()
+
+    if args.input_file is None:
+        raise ValueError("Input file not provided.")
+
+    convert_image(args.input_file, args.output_file, args.format)
+
+    return 0
 
 if __name__ == "__main__":
     main()
