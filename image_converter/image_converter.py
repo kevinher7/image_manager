dbe5742f50626d2f544b27e1b97a7198290be62d
Kevin Hernandez <kevinhernem@gmail.com>
2024-12-18 16:26:47 +0900

    feat: add convert_image to convert heic into png (as a default behaviour but can work with other formats)

--- image_converter/image_converter.py
+++ image_converter/image_converter.py
@@ -1,9 +1,33 @@
+import os
 import argparse
+
+from PIL import Image
+from pillow_heif import register_heif_opener
 
 from ..utils import get_image_path_from_name
 
-def convert_image(input_file, output_file=None):
-    return get_image_path_from_name(input_file)
+def convert_image(input_file, output_file=None, output_format="png"):
+    input_image_path = get_image_path_from_name(input_file)
+
+    file_extension = input_image_path.split(".")[-1].lower()
+
+    if file_extension == "heic":
+        register_heif_opener()
+        temp_img = Image.open(input_image_path)
+        output_image_path = ""
+
+        if output_file == None:
+            output_image_path = input_image_path.replace(".HEIC", f".{output_format.lower()}")
+        else:
+            output_image_path = f"{input_image_path.split('.', 1)[0]}.{output_format}"
+
+        temp_img.save(output_image_path)
+
+    else:
+        print("This file format is not supported yet:(")
+    
+    return 0
+
 
 def main():
     print(convert_image("IMG_1815"))
